7ac4b91b666fd4a7fd6793f382b1110188aac5c9
Jake Abramson <abramso@bc.edu>
2026-02-15 14:37:48 -0500

    Initial Commit

---
+++ Concentration/ConcentrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcentrationApp.swift
+//  Concentration
+//
+//  Created by Jake Abramson on 2/15/26.
+//
+
+import SwiftUI
+
+@main
+struct ConcentrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
